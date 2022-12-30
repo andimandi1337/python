1024cbbd73a4c98da3001b123feef8c39f5dcf12
andimandi <andersfreitag@hotmail.de>
2022-12-30 16:14:59 +0100

    .

--- angular to degree.py
+++ angular to degree.py
@@ -5,7 +5,7 @@ input = float(input("Enter the value you would like to convert: "))
 class Angular_to_Degrees:
     
     def Conversion(input):
-        result = (input%pi)*180
+        result = input*180
         return result
     
-print(Angular_to_Degrees.Conversion(input), "is your entered value (", input, ") converted to degrees.")
+print(Angular_to_Degrees.Conversion(input), "is your entered value (", input, "pi) converted to degrees.")
